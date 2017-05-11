885c3ac675b77fd3203f99a1d59e24213adc5f14
AndreaCirilloAC <andreacirilloac@gmail.com>
2017-05-11 22:04:16 +0200

    first commit

---
+++ R/palette_maker.R
@@ -0,0 +1,18 @@
+
+palette_maker <- function(image_path = NA, number_of_colors = 20){
+  if (is.na(image_path)){stop("you must provide a jpg image to build your palette from")}
+  painting     <- readJPEG(image_path)
+  dimension    <- dim(painting)
+  painting_rgb <- data.frame(
+    x = rep(1:dimension[2], each = dimension[1]),
+    y = rep(dimension[1]:1, dimension[2]),
+    R = as.vector(painting[,,1]),
+    G = as.vector(painting[,,2]),
+    B = as.vector(painting[,,3])
+  )
+  k_means        <- kmeans(painting_rgb[,c("R","G","B")], centers = number_of_colors, iter.max = 30)
+  colours_k      <- rgb(k_means$centers[k_means$cluster,])
+  colours_vector <- unique(colours_k)
+  show_col(colours_vector)
+  return(colours_vector)
+}
