a729494f341939f0bc6adf65e826baea43081553
AndreaCirilloAC <andreacirilloac@gmail.com>
2017-11-19 09:12:16 +0100

    updated doc

--- R/optimize_palette.R
+++ R/optimize_palette.R
@@ -13,7 +13,7 @@
 #' - subset of the palette according to the type of variable to be plotted: a spaced sample in case of categorical variables, an interpolation between two colours close to the mode of h for continous variables
 #' @author Andrea Cirillo
 #' @examples
-#' create_palette("inst/data/nascita_venere.jpg",number_of_colors = 20)
+#' create_palette("data/nascita_venere.jpg",number_of_colors = 20)
 #' @export
 optimize_palette <- function(rgb_raw_palette = NA,
                              number_of_colors =NA ,
